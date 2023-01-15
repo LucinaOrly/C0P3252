1afc37f8d86c1ce5bf5a58972e17c79a524c192f
LucinaOrly <l.orly528@outlook.com>
2023-01-15 13:44:48 -0500

    Rename, finish Q1, started Q2

---
+++ 1/Q1.java
@@ -0,0 +1,33 @@
+// 2.17
+// application that inputs three integers form the user and 
+// displayes the sum, average, product, smallest and largest 
+// of the numbers.
+import java.util.Scanner;
+
+public class Q1 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int a, b, c, sum, ave, prod, min, max;
+
+		System.out.print("Enter first integer: ");
+		a = input.nextInt();
+		System.out.print("Enter seccond integer: ");
+		b = input.nextInt();
+		System.out.print("Enter third integer: ");
+		c = input.nextInt();
+
+		sum = a + b + c;
+		ave = sum / 3;
+		prod = a * b * c;
+		min = Math.min(Math.min(a,b),c);
+		max = Math.max(Math.max(a,b),c);
+		
+		System.out.printf("Your three integers: %d %d %d%n", a, b, c);	
+		System.out.printf("Sum: %d+%d+%d= %d%n",a,b,c,sum);
+		System.out.printf("Average: %d%n", ave);
+		System.out.printf("Product: %d*%d*%d= %d%n",a,b,c,prod);
+		System.out.printf("Min: %d%n", max);
+		System.out.printf("Min: %d%n", min);
+	}
+}
---
+++ 1/Q2.java
@@ -0,0 +1,24 @@
+// 2.24
+// an application that reads five integers and determines 
+// and prints the largest and smallest integers in the group
+import java.util.Scanner;
+
+public class Q2 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int[] list = new int[5];
+		int min = Integer.MAX_VALUE, 
+			max = Integer.MIN_VALUE;
+
+		System.out.print("Enter 5 integers: ");
+		for(int i=0;i<5;i++) { 
+			list[i] = input.nextInteger();
+			min = Math.min(min, list[i]);
+			max = Math.max(max, list[i]);
+		}
+
+		System.out.printf("min: %d%n", min);
+		System.out.printf("max: %d%n", max);
+
+	}
+}
