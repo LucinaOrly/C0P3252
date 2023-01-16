203699b6eebb764343fab10e72662ea0ac8d4e98
LucinaOrly <l.orly528@outlook.com>
2023-01-15 19:23:26 -0500

    Finished project completely
    done <3

--- 1/Q3.java
+++ 1/Q3.java
@@ -1,1 +1,21 @@
 // 2.25
+// application that reads an integer and determines 
+// and prints whether it's odd or even
+
+import java.util.Scanner;
+
+public class Q3 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);	
+		int num; 
+		
+		System.out.print("Enter an integer: ");
+		num = input.nextInt();
+
+		System.out.printf("The integer %d is ", num);
+		if (num % 2 != 0) 				// odd 
+			System.out.println("odd!");
+		else						// even
+			System.out.println("even!");
+	}
+}
