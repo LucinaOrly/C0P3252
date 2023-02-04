b990c6f5f7a91f3fd9eeaa7cb2305f49363b8d88
John - Valencia-Londono <valencia@linprog7.cs.fsu.edu>
2023-02-04 11:22:23 -0500

    SumOf.java done

---
+++ 4/SumOf.java
@@ -0,0 +1,27 @@
+// 7.16
+// (Using the Enhanced for statement) Write an application that uses an
+// enhanced for statement tosum the double values passed by the command-line
+// arguments 
+// Honestly I am just going to copy part of one of my programs from the first assignment
+
+public class SumOf {
+	
+	public static void main(String[] args) {
+		double sum = 0.0, tmp;
+		String result = "";
+		for(String str : args) {
+			try {
+			    tmp = Double.parseDouble(str);
+				sum += tmp;	
+				result += str + " + ";
+			}
+			catch(NumberFormatException e) {
+				System.out.printf("Invalid input, has to be a double value (%s)%n",
+					e);		
+				return;
+			}
+		}
+
+		System.out.printf("%s = %f%n", result.substring(0,result.length()-1), sum);
+	}
+}
