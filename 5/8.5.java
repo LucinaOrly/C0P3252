b1ac0ec8649cbce4164b0266168ced8634b1b805
LucinaOrly <l.orly528@outlook.com>
2023-02-08 11:48:36 -0500

    starting Assignment4. Time2.java

---
+++ 5/8.5.java
@@ -0,0 +1,13 @@
+// 8.5
+// (Modifying the Internal Data Representation of a Class) It would be
+// perfectly reasonable for the Time2 class of Fig. 8.5 to represent  time
+// internally, as the number of seconds since midnight rather than the three
+// integer values hour, minute and second. Clients could use the same public
+// methods and get the same results. Modify the Time2 class of Fig. 8.5 to
+// implement the time as number of seconds since midnight and show that no
+// change is visible to the clients of the class.
+//
+// [Note: For Excercise 8.5, also include your modified version of Figure 8.6;
+// i.e., include all files needed to run your program.]
+
+
---
+++ 5/Time2.java
@@ -0,0 +1,78 @@
+// Fig. 8.5: Time2java
+
+public class Time2 {
+	private int second; // seconds after midnight
+	
+	// Time2 no-arg constructor
+	public Time2(){
+		this(0);
+	}
+
+	// Time2 consntructor, second supplied
+	public Time2(int second) {
+		setSecond(second);
+	}
+
+	// Time2 constructor, copy constructor
+	public Time2(Time2 time) {
+		setTime(time.second);
+	}
+
+	// setters
+	public void setTime(int hour, int minute, int second) {
+			
+	}
+	public void setHour(int hour) {
+		if(hour < 0 || hour >= 24)
+			throw IllegalArgumentException("hour must be between 0-23");
+		this.second %= 3600 + htos(hour);
+	}
+	public void setMinute(int minute) {
+		if(minute < 0 || minute >= 24)
+			throw IllegalArgumentException("minute must be between 0-59");
+		this.second = 
+	}
+	public void setSecond() {
+		if(hour < 0 || hour >= 24)
+			throw IllegalArgumentException("hour must be between 0-23");
+		this.second %= 3600 + htos(hour);
+	
+	}
+
+	// getters
+	public int getHour() {
+		return second / 3600;
+	}
+	public int getMinute() { 
+		return second / 60;
+	}
+	public int getSecond() {
+		return second % 60;
+	}
+	public int getSecondSinceMidnight() {
+		return second;
+	}
+
+	// convert to String in universal-time format
+	public String toUniversalString() {
+		return String.format("%02d:%02d:$02d",
+			getHour(),
+			getMinute(),
+			getSecond()
+		);
+	}
+
+	// convert to String in standar-time format
+	public String toString() {
+		// edit this later
+		return String();
+	}
+
+	// private helper functions
+	private static mtos(int m) {
+		return m * 60;
+	}
+	private static htos(int h) {
+		return h * 3600;
+	}
+}
