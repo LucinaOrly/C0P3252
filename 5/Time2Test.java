ec7cbdefc88c05f3a3a4231fe60c299fd8f56037
LucinaOrly <l.orly528@outlook.com>
2023-02-08 14:11:57 -0500

    sync

--- 5/Time2Test.java
+++ 5/Time2Test.java
@@ -10,4 +10,15 @@
 // [Note: For Excercise 8.5, also include your modified version of Figure 8.6;
 // i.e., include all files needed to run your program.]
 
+public class Time2Test {
+	public static void main(String[] args) {
+		Time2 t1 = new Time2();
+		Time2 t2 = new Time2(2);
+		Time2 t3 = new Time2(21, 34);
+		Time2 t4 = new Time2(12, 25, 42);
+		Time2 t5 = new Time2(t4);
 
+		Time2[] tarr = {t1,t2,t3,t4,t5};
+		
+	}
+}
